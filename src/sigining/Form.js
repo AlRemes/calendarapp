fb1f1acbd6c002932c8eb57ab1d5acccb55c8405
AlRemes Github <alpo.remes@gmail.com>
2022-04-30 21:36:21 +0300

    Finalizing ui of log inpage

--- src/sigining/Form.js
+++ src/sigining/Form.js
@@ -1,47 +1,67 @@
 import React, { useState, useEffect } from "react";
 
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import Buttons from "./Button.js";
-import { Button } from "@mui/material";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Box from '@mui/material/Box'
 
-import { useLocation } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from '@mui/icons-material/Add';
+import { fontSize } from "@mui/system";
 
-
-export default function Form(props) {
+export default function Form({handleClose, handleSave, open, handleClickOpen}) {
     // {title, setEmail, setPassword, handleData}
 
-    let location = useLocation();
-  console.log(location);
+
+  const [login, setLogin] = useState({
+    email:'', password:''
+  });
+
+  const inputChange = (event) => {
+    setLogin({...login, [event.target.name]:event.target.value})
+  }
 
   return (
     <Box component="form">
-      {/* <h1>{title} form</h1> */}
-      <Box>
-        <TextField
-          id="standard-basic"
-          label="email"
-          variant="standard"
-          value={props.email}
-          onChange={(event) => {
-            props.setEmail(event.target.value);
-          }}
-        />
-      </Box>
-      <Box>
-        <TextField
-          id="standard-basic"
-          label="password"
-          variant="standard"
-          placeholder="Password..."
-        //   value={password}
-          onChange={(event) => {
-            // setPassword(event.target.value);
-          }}
-        />
-      </Box>
-      <Button onClick={_=> console.log(props.location.state)}>Test</Button>
-      {/* <Buttons  title={title} handleData={handleData}/> */}
+     <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Make account with email</DialogTitle>
+        <DialogContent>
+
+          <TextField
+            margin="dense"
+            name="email"
+            label="email"
+            value={login.email}
+            onChange={inputChange}
+            fullWidth
+            variant="standard"
+
+          />
+          <TextField
+            margin="dense"
+            name="password"
+            value={login.password}
+            onChange={inputChange}
+            label="password"
+            fullWidth
+            variant="standard"
+          />
+              
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={() => handleSave(login)}>Save</Button>
+        </DialogActions>
+        
+      </Dialog>
+      <IconButton variant="outlined" onClick={handleClickOpen} style={{marginTop:50, fontSize:20}}>
+        <AddIcon />
+        <div>Create new account with email</div>
+      </IconButton>
+
     </Box>
   );
 }
--- src/sigining/Signup.js
+++ src/sigining/Signup.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 
 import { auth } from "../components.js/Firebase.js";
 
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
 
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
+import { useNavigate } from "react-router-dom";
 
-import {Link, useNavigate} from"react-router-dom";
-
-import Firebase from "../components.js/Firebase.js";
-
+import IconButton from "@mui/material/IconButton";
+import EmailIcon from "@mui/icons-material/Email";
+import GoogleIcon from '@mui/icons-material/Google';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 import {
   createUserWithEmailAndPassword,
@@ -17,87 +18,81 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   GithubAuthProvider,
-  signOut
+  signOut,
 } from "firebase/auth";
 import { Button } from "@mui/material";
 
-let user = '';
+import Form from "./Form.js";
+
+let user = "";
 
 const Signup = (props) => {
+  const navigate = useNavigate();
 
-const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
+  const move = () => {
+    navigate("/calendar", { replace: true });
+  };
 
-const move = () =>{
-  navigate('/calendar', { replace: true })
-}
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((re) => {
+        user = re.user.email;
+        move();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-const signInWithGoogle = () => {
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider)
-  .then((re) => {
-    user = re.user.email
-    move()
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-}
+  const signInWithGithub = () => {
+    const provider = new GithubAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((re) => {
+        user = re.user.email;
+        move();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-const signInWithGithub = () =>{
-  const provider = new GithubAuthProvider();
-  signInWithPopup(auth, provider)
-  .then((re) => {
-    user = re.user.email
-    move()
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-}
+  const handleSave = (login) => {
+    if (login.email.includes(".") && login.email.includes("@")) {
+      createUserWithEmailAndPassword(auth, login.email, login.password)
+        .then((re) => {
+          user = re.user.email;
+          alert("Account made");
+          setOpen(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      alert("Not a valid email");
+    }
+  };
 
-const createEmailAcc = () => {
-  if (email.includes('.') && email.includes('@')){
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((re) => {
-    user = re.user.email
-
-    console.log(re)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-} else{
-  alert('Not a valid email')
-}
-}
-
-const signInEmail = () => {
-signInWithEmailAndPassword(auth, email, password)
+  const signInEmail = () => {
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in 
+        // Signed in
         const user = userCredential.user;
-        if (userCredential){
-        move()
-      } else {
-        alert('Something went wrong. Are your login details correct?')
-      }
+        if (userCredential) {
+          move();
+        } else {
+          alert("Something went wrong. Are your login details correct?");
+        }
         // ...
       })
       .catch((error) => {
-        
         const errorCode = error.code;
         const errorMessage = error.message;
       });
-    }
-
-
-
-const handleData = (id) => {
-
-}
+  };
 
   const [open, setOpen] = useState(false);
 
@@ -110,48 +105,57 @@ const handleData = (id) => {
   };
 
   return (
-    <>    
-    <Box>
-    <Box>
-        <TextField
-          id="standard-basic"
-          label="email"
-          variant="standard"
-          value={email}
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
+    <>
+      <Box style={{flex: 1, display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+        <Box>
+        <h3 style={{color:'grey'}}>Hello! Create account or login</h3>
+
+        <Box>
+          <TextField
+            id="standard-basic"
+            label="email"
+            variant="standard"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </Box>
+        <Box>
+          <TextField
+            id="standard-basic"
+            label="password"
+            variant="standard"
+            placeholder="Password..."
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </Box>
+        <IconButton variant="outlined" onClick={signInEmail}>
+          <EmailIcon />
+          <div style={{ fontSize: "70%" }}> Sign in with Email account</div>
+        </IconButton>
+
+        <Box>
+        <IconButton variant="outlined" onClick={signInWithGoogle}>
+            <GoogleIcon />
+        </IconButton>
+        <IconButton variant="outlined" onClick={signInWithGoogle}>
+            <GitHubIcon />
+        </IconButton>
+          </Box>
+        </Box>
+        <Box>
+        <Form
+          handleClose={handleClose}
+          handleClickOpen={handleClickOpen}
+          open={open}
+          handleSave={handleSave}
         />
+        </Box>
       </Box>
-      <Box>
-        <TextField
-          id="standard-basic"
-          label="password"
-          variant="standard"
-          placeholder="Password..."
-          value={password}
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-      </Box>
-    <Button
-    variant="outlined" onClick={signInEmail} style={{marginTop:10}}>
-        Sign in with Email account
-    </Button>
-    <Button
-    variant="outlined" onClick={signInWithGoogle} style={{marginTop:10}}>
-        Sign in with Google
-    </Button>
-    <Button
-    variant="outlined" onClick={signInWithGithub} style={{marginTop:10}}>
-        Sign in with Github
-    </Button>
-    <Button
-    variant="outlined" onClick={createEmailAcc} style={{marginTop:10}}>
-        Create Email account
-    </Button>
-    </Box>
     </>
   );
 };
